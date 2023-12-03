579eedb90936cbf34b4d3796c9fc6c940dbd99b3
Lam <lttlam1710@gmail.com>
2023-12-03 13:14:49 +0700

    WallJumpFinal

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,5 @@
+﻿using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,22 +13,32 @@ public class Player : MonoBehaviour
     private bool yInput;
     private int jumpMax = 2;
     private int jumpCount;
+    private bool isFacingRight = true;
+    private bool canMove;
+    private int facingDir =1;
+
+    [Header("Check Collision")]
+    public Transform wallCheck;
+    public Transform groundCheck;
     private bool isGrounded;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private float wallCheckDistance;
+    private bool isWallDetected;
     [SerializeField] private LayerMask whatIsGround;
+    public BoxCollider2D groundCheckCollider; 
+    public BoxCollider2D wallCheckCollider;   
+
 
     [Header("Wall Slide")]
-    private bool isWallDetected;
     private bool isWallSliding;
     [SerializeField] private float wallSlidingSpeed;
 
+    [Header("Wall Jump")]
+    private bool isWallJump;
+    [SerializeField] private Vector2 wallJumpDirection;
 
 
     private Animator anim;
 
-    private bool isFacingRight = true;
-    private int facingDirection;
+
 
     void Start()
     {
@@ -39,48 +51,76 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        CollisionChecks();          // Check Ground,
-        InputCheck();               // Input from player
-        Move();                     // Move (rb.velocity * moveInput)
-        DoubleJump();               // Jumpcount > 0 and isGround
-        AnimController();           // Animation setup
-        FlipController();           // Facing = Rotate transform
-        WallSliding();              // Walldetected = true and velocity.y < 0 then WallSlide
+        CollisionChecks();
+        InputCheck();
+        Move();
+        DoubleJump();
+        AnimController();
+        FlipController();
 
+        if (isWallDetected)
+        {
+            WallSliding();
+            if (yInput && isWallSliding)
+            {
+                WallJumping();
+            }
+        }
+        else
+        {
+            isWallSliding = false;
+            if (isWallJump)
+            {
+                // Cho phép một khoảng thời gian ngắn để nhân vật di chuyển sau wall jump
+                StartCoroutine(EnableMovementAfterDelay());
+            }
+        }
 
+        if (isGrounded)
+        {
 
+            canMove = true;
+            isWallJump = false; // Đặt isWallJump thành false khi chạm đất
+        }
+
+    }
+
+    private IEnumerator EnableMovementAfterDelay()
+    {
+        yield return new WaitForSeconds(0.1f); // Đợi 0.1 giây trước khi cho phép di chuyển
+        canMove = true;
+        isWallJump = false; // Đặt isWallJump thành false sau khi rời khỏi tường
     }
 
     private void WallSliding()
     {
         if (isWallDetected && rb.velocity.y < 0)
         {
-
             isWallSliding = true;
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * wallSlidingSpeed);
-            
-                
-
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -wallSlidingSpeed));
         }
-        if (!isWallDetected)
+        else
         {
-
             isWallSliding = false;
-
-            
         }
     }
 
-    private void AnimController()
+    private void WallJumping()
     {
+        if (isWallSliding)
+        {
+            
+            rb.velocity = new Vector2(wallJumpDirection.x * -facingDir, wallJumpDirection.y);
+            canMove = false;
+            isWallJump = true;
+            jumpCount = jumpMax;
 
-        anim.SetFloat("xInput", rb.velocity.x);
 
-        anim.SetFloat("yInput", rb.velocity.y);
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetBool("isSliding", isWallSliding);
-
+        }
+        
     }
+
+   
 
     private void DoubleJump()
     {
@@ -94,12 +134,6 @@ public class Player : MonoBehaviour
             Jump();
             jumpCount -= 1;
         }
-        /*if (isWallDetected)
-        {
-            jumpCount++;
-        }*/
-
-
     }
 
     private void InputCheck()
@@ -108,68 +142,74 @@ public class Player : MonoBehaviour
         yInput = Input.GetButtonDown("Jump");
 
         if (Input.GetAxis("Vertical") < 0)
-        {
-
             isWallDetected = false;
-        }
+
     }
 
     private void Move()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        if(canMove)
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+
+        }
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (isWallSliding)
+        {
+            WallJumping();
+        }
+        else if (isGrounded && yInput)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            
+        }
+        else if (!isGrounded && yInput && jumpCount > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--; // Giảm jumpCount mỗi khi nhảy trong không trung
+        }
     }
 
 
     private void FlipController()
     {
-        if (isFacingRight && xInput < 0) // Faceing Right = false and Move to Left
-        {
+        if (isFacingRight && rb.velocity.x < 0) // Faceing Right = false and Move to Left
             Flip();
-        }
-        else if (!isFacingRight && xInput > 0) // Faceing Right = true and Move to Right
-        {
+        else if (!isFacingRight && rb.velocity.x > 0) // Faceing Right = true and Move to Right
             Flip();
-        }
+
     }
 
     private void Flip()
     {
-        facingDirection = facingDirection * -1; //Facing Dir to Left
+        facingDir = facingDir * -1;
         isFacingRight = !isFacingRight;         // Facing Left
         transform.Rotate(0, 180, 0);            // Rotate to Left
     }
+    private void AnimController()
+    {
 
+        anim.SetFloat("xInput", rb.velocity.x);
+
+        anim.SetFloat("yInput", rb.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isSliding", isWallSliding);
+
+    }
     private void CollisionChecks()
     {
-        //Ground check line (position, Vector.down, Vector lenght, Layer)
-        isGrounded = Physics2D.Raycast(transform.position,      // form position of line middle character
-                                        Vector2.down,           // to Vector draw to down
-                                        groundCheckDistance,    // Vector length by float
-                                        whatIsGround);          // Layer of Ground set in Unity layer
-        //Wall check line (position, Vector.right, Vector lenght, Layer)
-        isWallDetected = Physics2D.Raycast(transform.position,
-                                        Vector2.right,
-                                        wallCheckDistance,
-                                        whatIsGround);
+        Vector2 groundCheckSize = groundCheckCollider.size;
+        Vector2 wallCheckSize = wallCheckCollider.size;
+        //Ground check box
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, whatIsGround);
 
+        //Wall check box
+        isWallDetected = Physics2D.OverlapBox(wallCheck.position, wallCheckSize, 0, whatIsGround);
+        
     }
 
-    private void OnDrawGizmos()
-    {
-        // DrawLine (position A from, to position B)
-        Gizmos.DrawLine(transform.position,                     // position A
-                        new Vector2(transform.position.x,       // position B (x, y to vector Length can input)
-                                    transform.position.y -
-                                    groundCheckDistance));
 
-        // DrawLine (position A from, to position B)
-        Gizmos.DrawLine(new Vector2(transform.position.x + wallCheckDistance, transform.position.y), transform.position);
-
-
-    }
 }
