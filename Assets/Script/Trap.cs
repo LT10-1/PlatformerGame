67e42a01d245aa59d74f84beb0515a967c14ee9e
Lam <lttlam1710@gmail.com>
2023-12-09 13:57:49 +0700

    TrapCleanCode

---
+++ Assets/Script/Trap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+
+        Player playerCollider = collision.GetComponent<Player>();
+        if (playerCollider != null)
+        {
+            if (!playerCollider.isRoll)
+                playerCollider.PlayerHit();
+
+        }
+    }
+}
